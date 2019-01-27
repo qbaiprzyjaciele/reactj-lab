b876fd8dfb2d38eea388575e8aaf9a92dab8fa8e
Jakub Smorąg <qbaiprzyjaciele@gmail.com>
2019-01-28 00:35:39 +0100

    tutuorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi how are you?</h1>
+        <p>it works</p>
+        <Person></Person>
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = () => {
+    return <p> a person I am {Math.floor(Math.random() * 30)} years world!</p>
+};
+
+
+
+export default person;
